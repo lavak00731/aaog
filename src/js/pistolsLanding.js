a1c1f7280047251914f9d2d26fa7c444fdb114fb
ezelavaca <ezequiel.lavaca@endava.com>
2023-01-27 18:31:49 -0300

    Changing filters in pistols, adding artillery pictures

--- src/js/pistolsLanding.js
+++ src/js/pistolsLanding.js
@@ -3,34 +3,13 @@ import "./../styles/products.css";
 import "./../styles/contact.css";
 import "./../styles/productLanding.css";
 import { Shared } from "./shared";
-import { addListForFilteringByName, filterByName, resetSearch, addFilterByFeature, filterByFeature } from './filters';
+import { addFilterByFeature, filterByFeature, resetFilterByFeature } from './filters';
 Shared();
-addListForFilteringByName('pistolsandrifles', 'pist');
 addFilterByFeature('pistolsandrifles')
 const productList = document.querySelectorAll('.aaog-product-wrapper');
-const searchTrigger = document.querySelector('#searchTrigger');
-const inputElem = document.querySelector('#inputSearch');
 const featureTrigger = document.querySelectorAll('input[type="radio"]');
-const filterWrapper = document.querySelector('#filters');
-searchTrigger.addEventListener('click', (e)=>{ 
-    if(inputElem.value.length > 0) {
-        filterByName(inputElem.value, productList);
-    } 
-}, false);
-inputElem.addEventListener('input', ()=>{
-    if(inputElem.value.length === 0) {
-        resetSearch(productList);
-    }
-}, false);
+const resetTrigger = document.querySelector('#resetTrigger');
+resetTrigger.addEventListener('click', () => resetFilterByFeature(productList), false);
 featureTrigger.forEach((feature)=>{
     feature.addEventListener('change', (e)=> filterByFeature(e.target.value, productList), false)
-})
-document.querySelector('#filterTrigger').addEventListener('click', ()=>{
-    if(filterWrapper.hasAttribute('hidden')) {
-        filterWrapper.removeAttribute('hidden');
-    } else {
-        filterWrapper.setAttribute('hidden', true);
-    }
-})
-
-
+});
