a382ec357802362e5e9e939977f5beb93e2689eb
Ezequiel <eolavaca@gmail.com>
2023-08-06 21:13:34 -0300

    form sending good way

--- src/js/product.js
+++ src/js/product.js
@@ -3,11 +3,16 @@ import 'photoswipe/style.css';
 import "./../styles/styles.css";
 import "./../styles/contact.css";
 import "./../styles/product.css";
+import { ValidAccess } from './validAccess';
 import { Shared } from "./shared";
 import { shareProduct } from './shareProduct';
+import errorMsg from './../errorMsg.json';
 import { modal } from './modal';
+import img from './../imagenes/loadingsmall.gif';
+
 Shared();
 shareProduct();
+
 if(document.querySelector('html[lang="es"]')) {
     const lightbox = new PhotoSwipeLightbox({
       gallery: '#product-gallery',
@@ -32,9 +37,20 @@ if(document.querySelector('html[lang="es"]')) {
 (function(){
   const modalFunc = modal();
   const btnTrigger = document.querySelectorAll('.aaog-modal-trigger');
-  const btnClose = document.querySelectorAll('.aaog-close-modal');
+  const btnClose = document.querySelectorAll('.aaog-close-modal, .aaog-final-btn');
   const formElem = document.querySelector('form');
-  const heading = document.querySelector('h1').textContent
+  const heading = document.querySelector('h1').textContent;
+  const reseteableElems = document.querySelectorAll('.aaog-modal-body .aaog-overflow-hidden');
+  const closeAndReset = () => {
+    modalFunc.closeModal('#prodId');
+      formElem.reset();
+      reseteableElems.forEach((elem)=>{
+        if(!elem.hasAttribute('hidden')){
+          elem.setAttribute('hidden', true);
+          formElem.removeAttribute('hidden');
+        }
+      })
+  }
   btnTrigger.forEach(( btn )=>{
     btn.addEventListener('click', function(){
       modalFunc.openModal('#prodId', btn)
@@ -43,8 +59,19 @@ if(document.querySelector('html[lang="es"]')) {
   });
   btnClose.forEach(( btn )=>{
     btn.addEventListener('click', function(){
-      modalFunc.closeModal('#prodId');
-      formElem.reset()
+      closeAndReset();
     }, false);
   });
-})()
+})();
+(function(){
+   const form = new ValidAccess({
+      formId: "prodForm",
+      url: 'https://reqres.in/api/users', 
+      success: 'aaog-form-success', 
+      error: 'aaog-form-error', 
+      msgUrl: errorMsg,
+      bannerClass: 'aaog-alert-banner',
+      fieldError: 'aaog-error',
+      loadingImg: img
+   })
+})();
