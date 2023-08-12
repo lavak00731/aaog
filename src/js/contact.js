ca4f37222c989f234c87b5dd5489a20f115b67ea
Ezequiel <eolavaca@gmail.com>
2023-08-12 10:10:29 -0300

    contact form with validation

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,5 +1,19 @@
 import "./../styles/styles.css";
 import "./../styles/contact.css";
 import { Shared } from './shared';
-
+import errorMsg from './../errorMsg.json';
+import { ValidAccess } from './validAccess';
+import img from './../imagenes/loadingsmall.gif';
 Shared();
+(function(){
+    const form = new ValidAccess({
+       formId: "contactForm",
+       url: 'https://reqre54s.in/api/unknown/23', 
+       success: 'aaog-form-success', 
+       error: 'aaog-form-error', 
+       msgUrl: errorMsg,
+       bannerClass: 'aaog-alert-banner',
+       fieldError: 'aaog-error',
+       loadingImg: img
+    })
+ })();
